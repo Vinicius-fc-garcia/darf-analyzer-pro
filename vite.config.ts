7a578b61809d2b83d4056370ad3ad96ab80c5bf7
Vinicius-fc-garcia <vinicius.fc.garcia@gmail.com>
2026-02-13 15:52:50 -0300

    Configure base path and build settings in Vite
    Set base path and build target for modern browsers.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/darf-analyzer-pro/',
+  build: {
+    target: 'esnext', // Otimiza para navegadores modernos e evita problemas com top-level await se existirem
+  }
 })
